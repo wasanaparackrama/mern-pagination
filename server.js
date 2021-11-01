4ad24bfddf3dbea28a2cfc2e7b0f95ea9af75dff
wsna <wsna524@gmail.com>
2021-11-01 21:19:39 +0530

    final deplyment

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 require("dotenv").config({ path: "./config.env" });
 const express = require("express");
 const connectDB = require("./config/db");
+const path = require('path');
 const postRoutes = require("./routes/postRoutes");
 
 connectDB();
@@ -10,6 +11,21 @@ const app = express();
 app.use(express.json());
 
 app.use("/api/v1/posts", postRoutes);
+
+if(process.env.NODE_ENV ==="production"){
+    app.use(express.static(path.join(__dirname,'/client/build')));
+
+    app.get('*',(req,res) => {
+        res.sendFile(path.join(__dirname,'client','build','index.html'));
+    })
+}
+    else{
+        app.get('/',(req,res)=>{
+            res.send("Api running");
+        })
+    }
+
+
 
 const PORT = process.env.PORT;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
